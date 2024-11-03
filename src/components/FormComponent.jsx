280a54df022e3bf846bd36b1c8b1a7b6a5f92513
Dharyl <programmingdha@gmail.com>
2024-11-04 06:55:24 +0800

    Form Handling

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FormComponent.css";
 import TagComponent from "./TagComponent";
 
 const FormComponent = () => {
+  const [taskData, setTaskData] = useState({
+    task: "",
+    status: "todo",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setTaskData((previosValue) => {
+      return { ...previosValue, [name]: value };
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(taskData);
+  };
+
   return (
     <>
       <header className="app_header_section">
-        <form>
-          <input type="text" className="task_input" placeholder="Enter Task" />
+        <form onSubmit={handleSubmit}>
+          <input
+            name="task"
+            type="text"
+            className="task_input"
+            placeholder="Enter Task"
+            onChange={handleChange}
+          />
 
           <div className="task_form_bottom_line">
             <div>
@@ -18,7 +42,11 @@ const FormComponent = () => {
             </div>
 
             <div>
-              <select className="task_status">
+              <select
+                name="status"
+                className="task_status"
+                onChange={handleChange}
+              >
                 <option value="todo">To-Do</option>
                 <option value="working">Working</option>
                 <option value="done">Done</option>
